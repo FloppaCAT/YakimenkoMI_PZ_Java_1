4406ea5ebec0ae59127f3e4a81205a8a2b533ce3
FloppaCAT <ghastlive@gmail.com>
2021-04-04 23:33:08 +0300

    4. Считывание с клавиатуры, нахождение среднего арифметического, деление на 2 и округление вниз

---
+++ src/com/YakimenkoPZ/YakimenkoPZ_4.java
@@ -0,0 +1,31 @@
+package com.YakimenkoPZ;
+
+import java.util.Scanner;
+
+public class YakimenkoPZ_4 {
+    public static void main(String[] args) {
+        //Объявляем массив
+        int Arr[] = new int[3];
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Введите 3 любых числа");
+        //Наполняем массив числами с клавиатуры
+        for (int i=0;i<3;i++){
+            Arr[i] = scan1.nextInt();
+        }
+        double average = 0;
+        double sum = 0;
+        //Считаем среднее арефметическое
+        for (int i=0;i<Arr.length;i++){
+            sum+=Arr[i];
+        }
+        average = sum/Arr.length;
+        System.out.println("Среднее арефметическое введенных чисел = " + average);
+        //Округляем вниз
+        average = average/2;
+        double FloorAverage = Math.floor(average);
+        System.out.println("Разделили результат на 2 и округлили вниз = " + FloorAverage);
+        if (FloorAverage>3)
+            System.out.println("Программа выполнена корректно");
+
+    }
+}
