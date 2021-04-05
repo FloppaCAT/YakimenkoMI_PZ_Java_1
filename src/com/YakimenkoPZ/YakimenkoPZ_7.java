d42c636ab7a88dfbbea9fb91ece688fdecb460ff
FloppaCAT <ghastlive@gmail.com>
2021-04-05 23:16:15 +0300

    7. Перебор массива и сравнение с константами

---
+++ src/com/YakimenkoPZ/YakimenkoPZ_7.java
@@ -0,0 +1,36 @@
+package com.YakimenkoPZ;
+
+import java.util.Scanner;
+
+public class YakimenkoPZ_7 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 5;
+        int z = 11;
+        System.out.println("Введите рамер массива");
+        Scanner scan1 = new Scanner(System.in);
+        int a = scan1.nextInt();
+        int[] arr = new int[a];
+
+        System.out.println("Заполните массив, вводя значения с клавиатуры");
+        for(int i=0; i<arr.length;i++){
+            arr[i] = scan1.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            if (arr[i] == x){
+                System.out.println("Данное значение имеется в константах");
+                break;
+            } else if(arr[i] == y){
+                System.out.println("Данное значение имеется в константах");
+                break;
+            } else if (arr[i] == z){
+                System.out.println("Данное значение имеется в константах");
+                break;
+            }
+            if (i == arr.length-1){
+                System.out.println("Нет совпадений с константами");
+            }
+        }
+
+    }
+}
